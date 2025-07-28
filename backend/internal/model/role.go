38a71e5e5613881e674ff1318d1836892bdc07d6
LiuKun/Windows <liukunup@163.com>
2025-07-28 22:16:17 +0800

    refactor: 优化role表

--- backend/internal/model/role.go
+++ backend/internal/model/role.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Role struct {
 	gorm.Model
 
-	Name       string `gorm:"column:name;type:varchar(255);uniqueIndex;comment:Custom Name"`
-	CasbinRole string `gorm:"column:casbin_role;type:varchar(255);uniqueIndex;comment:Casbin Role"`
+	Name       string `gorm:"column:name;type:varchar(64);uniqueIndex:uniq_name;comment:'角色名称(用于显示)'"`
+	CasbinRole string `gorm:"column:casbin_role;type:varchar(128);uniqueIndex:uniq_casbin_role;comment:'Casbin角色标识'"`
 }
 
 func (m *Role) TableName() string {
