c10c81e90fe5f81a800f0fadf9250fc11df0c642
LiuKun/Windows <liukunup@163.com>
2025-07-28 21:54:45 +0800

    refactor: 优化api表

--- backend/internal/model/api.go
+++ backend/internal/model/api.go
@@ -5,10 +5,10 @@ import "gorm.io/gorm"
 type Api struct {
 	gorm.Model
 
-	Group  string `gorm:"column:group;type:varchar(255);not null;comment:'分组'"`
-	Name   string `gorm:"column:name;type:varchar(255);not null;comment:'名称'"`
-	Path   string `gorm:"column:path;type:varchar(255);not null;comment:'路径'"`
-	Method string `gorm:"column:method;type:varchar(255);not null;comment:'方法'"`
+	Group  string `gorm:"column:group;type:varchar(100);not null;index;comment:'API分组'"`
+	Name   string `gorm:"column:name;type:varchar(255);not null;comment:'API名称'"`
+	Path   string `gorm:"column:path;type:varchar(512);not null;comment:'API路径'"`
+	Method string `gorm:"column:method;type:varchar(10);not null;index;check:method IN ('GET','POST','PUT','DELETE','PATCH','HEAD','OPTIONS');comment:'HTTP请求方法'"`
 }
 
 func (m *Api) TableName() string {
